2631cc4cb4c8303c33fda641f206bf5ef52b1cde
Keivon <keivonproctor@hotmail.com>
2019-12-28 23:35:56 -0400

    Updates

---
+++ precache-manifest.ed3fc8d9b207e094082e9cd3ee7c333e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de3842dfefc2d16c13205e8821e832d",
+    "url": "/Keivon/Car_Rental/index.html"
+  },
+  {
+    "revision": "d169b0aaef77a396b2cf",
+    "url": "/Keivon/Car_Rental/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a0640dd92217e6884ea",
+    "url": "/Keivon/Car_Rental/static/js/2.babb5de4.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Keivon/Car_Rental/static/js/2.babb5de4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d169b0aaef77a396b2cf",
+    "url": "/Keivon/Car_Rental/static/js/main.e53f3b88.chunk.js"
+  },
+  {
+    "revision": "f34a6d0ce5824c29f82e",
+    "url": "/Keivon/Car_Rental/static/js/runtime-main.c3b437c0.js"
+  },
+  {
+    "revision": "c01d517df7a4a8b01f9decc57fa77a64",
+    "url": "/Keivon/Car_Rental/static/media/jarisShop.c01d517d.jpg"
+  }
+]);
